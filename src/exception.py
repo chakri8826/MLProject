fa8d56e15c68294f2393defa47230a9740cfcd50
chakri8826 <kommuchakri8826@gmail.com>
2026-08-10 10:40:49 +0530

    Deployment configuration of EBS

--- src/exception.py
+++ src/exception.py
@@ -18,4 +18,3 @@ class CustomException(Exception):
     # Python automatically calls this.
     def __str__(self):
         return self.error_message
-    
--- src/logger.py
+++ src/logger.py
@@ -3,6 +3,8 @@ import os
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# os.getcwd() -> C:\Users\chakri\MLProject "logs" -> A folder name.
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
